607d437e711d40694a3d024fa18e7f8b8d36f03b
rudhravyas <91895312+rudhravyas@users.noreply.github.com>
2023-05-16 11:26:10 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import "./App.css";
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const fetchData = async () => {
+    const response = await fetch('https://www.terriblytinytales.com/test.txt');
+    const text = await response.text();
+    const words = text.split(/\s+/);
+    const counts = {};
+    words.forEach((word) => {
+      counts[word] = (counts[word] || 0) + 1;
+    });
+    const sorted = Object.entries(counts).sort((a, b) => b[1] - a[1]).slice(0, 20);
+    const chartData = sorted.map(([word, count]) => ({ word, count }));
+    setData(chartData);
+  };
+
+  const exportData = () => {
+    const csv = data.map(({ word, count }) => `${word},${count}`).join('\n');
+    const blob = new Blob([csv], { type: 'text/csv' });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = 'histogram.csv';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div id='div'>
+      <h2>TTT Assignment</h2>
+      <button className='btn' onClick={fetchData}>Submit</button>
+      {data.length > 0 && (
+        <>
+        <div className='chart'>
+          <BarChart width={600} height={400} data={data}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="word" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="count" fill="#e89988" />
+          </BarChart>
+        </div>
+          <button className="btn" onClick={exportData}>Export</button>
+      </>
+      )}
     </div>
   );
 }
